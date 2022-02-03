39334d23bca507d0d6937b7a6cd93bc32568ae99
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-02-03 16:10:17 -0400

    emoji

--- src/app.ts
+++ src/app.ts
@@ -20,14 +20,10 @@ createConnection()
 
 		app.listen(app.get('port'), () => {
 			console.log(`${key} corriendo en el puerto http://localhost:${app.get('port')} leoM   `);
-			console.log('_________');
-			console.log('|       |');
-			console.log('| ()_() |');
-			console.log(`| (o.o) |`);
-			console.log('| (|_|)*|');
-			console.log('|_______|');
-			console.log('| DB OK |');
-			console.log('|_______|');
+			console.log('╔═══╗ ♪');
+			console.log('║███║ ♫');
+			console.log('║ (●) ♫');
+			console.log('╚═══╝♪♪');
 		});
 	})
 	.catch((err) => console.log('DB ERR', err));
