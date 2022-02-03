694bc57941ad87539132a39310763c59fc4b2ecf
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-02-03 16:29:40 -0400

    logo init

--- src/app.ts
+++ src/app.ts
@@ -19,11 +19,22 @@ createConnection()
 		const { app, key } = sv;
 
 		app.listen(app.get('port'), () => {
-			console.log(`${key} Run in http://localhost:${app.get('port')}`);
-			console.log('╔═══╗ ♪');
-			console.log('║███║ ♫');
-			console.log('║ (●) ♫');
-			console.log('╚═══╝♪♪');
+			console.log(`${key}`);
+			console.log('            ________');
+			console.log('    o      |   __   |');
+			console.log(`      \_ O |  |__|  |`);
+			console.log('    ____/  |___WW___|');
+			console.log('   __/   /     || ');
+			console.log('               || ');
+			console.log('               || ');
+			console.log('_______________||________________ ');
+			console.log(`Run in http://localhost:${app.get('port')}`);
 		});
 	})
 	.catch((err) => console.log('DB ERR', err));
+/*
+	console.log('   ╔═══╗ ♪');
+	console.log('   ║███║ ♫');
+	console.log('   ║ (●) ♫');
+	console.log('   ╚═══╝♪♪');
+	*/
