56f424d36233243d2e57d13fb133476a23eb0121
Armando J. Rivas Caballero <arivas@tranred.com.ve>
2022-03-04 13:57:04 -0400

    merge conflicT

--- src/services/files/Middlewares/token/respOk.ts
+++ src/services/files/Middlewares/token/respOk.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import { Api } from 'interfaces';
+import jwt from 'jsonwebtoken';
 const Key: string = process.env.KEY || '_secreto';
 
-const respOk = (req: Request<any, Api.resp>, res: Response<Api.resp>, msg: Api.resp<any>) => {
+const respOk = (req: Request<any, Api.Resp>, res: Response<Api.Resp>, msg: Api.Resp<any>) => {
 	const { token }: any = req.headers;
 	msg.info.token = jwt.sign(token, Key, { expiresIn: 60 * 30 });
 
