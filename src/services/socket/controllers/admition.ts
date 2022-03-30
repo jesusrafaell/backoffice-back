8eef8d70ca4b39e3b4c1062563ffdfda2c1e4624
Aldrin <aetours.ca@gmail.com>
2022-03-30 14:40:42 -0400

    Aldrin vuelve a arreglar

--- src/services/socket/controllers/admition.ts
+++ src/services/socket/controllers/admition.ts
@@ -193,58 +193,63 @@ export const listSolic = async () => {
 	// 	/*sql*/ `SELECT * FROM [MilPagos].[dbo].[fm_status] where id_department = 1 and id_status_request = 1`
 	// );
 
-		const query = await getRepository(fm_status).find({
-			where: { id_status_request: 1, id_department: 4, id: Not(In(ids)) },
-			take: 50,
-			order: {
-				id: 'ASC',
-			},
-			relations: [
-				'id_request',
-				'id_request.id_client',
-				'id_request.id_client.id_location',
-				'id_request.id_client.id_location.id_estado',
-				'id_request.id_client.id_location.id_municipio',
-				'id_request.id_client.id_location.id_ciudad',
-				'id_request.id_client.id_location.id_parroquia',
-				'id_request.id_client.rc_ident_card',
-				'id_request.id_client.id_ident_type',
-				//
-				'id_request.rc_planilla',
-				'id_request.rc_planilla.id_photo',
-				'id_request.id_valid_request',
-				'id_request.pos',
-				'id_request.pos.id_location',
-				'id_request.pos.id_location.id_estado',
-				'id_request.pos.id_location.id_municipio',
-				'id_request.pos.id_location.id_ciudad',
-				'id_request.pos.id_location.id_parroquia',
-				//
-				'id_request.id_commerce',
-				'id_request.id_commerce.id_ident_type',
-				'id_request.id_commerce.id_activity',
-				'id_request.id_commerce.id_location',
-				'id_request.id_commerce.id_location.id_estado',
-				'id_request.id_commerce.id_location.id_municipio',
-				'id_request.id_commerce.id_location.id_ciudad',
-				'id_request.id_commerce.id_location.id_parroquia',
-				'id_request.id_commerce.banks',
-				'id_request.id_commerce.rc_constitutive_act',
-				'id_request.id_commerce.rc_constitutive_act.id_photo',
-				'id_request.id_commerce.rc_rif',
-				'id_request.id_commerce.rc_special_contributor',
-				'id_request.id_commerce.id_aci',
-				//
-				'id_request.id_product',
-				'id_request.id_type_request',
-				'id_request.id_request_origin',
-				//
-				'id_request.rc_ref_bank',
-				'id_request.rc_comp_dep',
-				'id_request.id_payment_method',
-				'id_request.id_type_payment',
-			],
-		});
+	let ids = [
+		...solictudes.map((solictude) => solictude.id),
+		...solictudesTrabajando.map((solictude) => solictude.id),
+	];
+
+	const query = await getRepository(fm_status).find({
+		where: { id_status_request: 1, id_department: 4, id: Not(In(ids)) },
+		take: 50,
+		order: {
+			id: 'ASC',
+		},
+		relations: [
+			'id_request',
+			'id_request.id_client',
+			'id_request.id_client.id_location',
+			'id_request.id_client.id_location.id_estado',
+			'id_request.id_client.id_location.id_municipio',
+			'id_request.id_client.id_location.id_ciudad',
+			'id_request.id_client.id_location.id_parroquia',
+			'id_request.id_client.rc_ident_card',
+			'id_request.id_client.id_ident_type',
+			//
+			'id_request.rc_planilla',
+			'id_request.rc_planilla.id_photo',
+			'id_request.id_valid_request',
+			'id_request.pos',
+			'id_request.pos.id_location',
+			'id_request.pos.id_location.id_estado',
+			'id_request.pos.id_location.id_municipio',
+			'id_request.pos.id_location.id_ciudad',
+			'id_request.pos.id_location.id_parroquia',
+			//
+			'id_request.id_commerce',
+			'id_request.id_commerce.id_ident_type',
+			'id_request.id_commerce.id_activity',
+			'id_request.id_commerce.id_location',
+			'id_request.id_commerce.id_location.id_estado',
+			'id_request.id_commerce.id_location.id_municipio',
+			'id_request.id_commerce.id_location.id_ciudad',
+			'id_request.id_commerce.id_location.id_parroquia',
+			'id_request.id_commerce.banks',
+			'id_request.id_commerce.rc_constitutive_act',
+			'id_request.id_commerce.rc_constitutive_act.id_photo',
+			'id_request.id_commerce.rc_rif',
+			'id_request.id_commerce.rc_special_contributor',
+			'id_request.id_commerce.id_aci',
+			//
+			'id_request.id_product',
+			'id_request.id_type_request',
+			'id_request.id_request_origin',
+			//
+			'id_request.rc_ref_bank',
+			'id_request.rc_comp_dep',
+			'id_request.id_payment_method',
+			'id_request.id_type_payment',
+		],
+	});
 
 	// console.log('Query para SOlicitud', query);
 
