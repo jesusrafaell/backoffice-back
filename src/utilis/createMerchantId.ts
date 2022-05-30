51730d9eada0161af55eca3f240667e5fcb10e51
Jesus <jesusrafaellh@gmail.com>
2022-05-29 23:29:47 -0400

    domingo

--- src/utilis/createMerchantId.ts
+++ src/utilis/createMerchantId.ts
@@ -1,4 +1,4 @@
 export const createMerchantId = (value1: string, id: number) => {
-	const merchantId = `7${Number(value1)}${11000 + (id + 200)}`;
+	const merchantId = `7${Number(value1)}${11000 + (id + 320)}`;
 	return merchantId;
 };
