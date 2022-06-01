20713ef10f5e500fcc5439438d8b17ca8b583649
Jesus <jesusrafaellh@gmail.com>
2022-06-01 19:37:34 -0400

    local

---
+++ src/services/office/router/seguridad/seguridad.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const Seguridad: Router = Router();
+//
+//Seguridad.route('/departments').get(getDepartments);
+//
+//Seguridad.route('/permisos').get(getPermisos);
+//
+//Seguridad.route('/roles').get(getRoles);
+
+export default Seguridad;
